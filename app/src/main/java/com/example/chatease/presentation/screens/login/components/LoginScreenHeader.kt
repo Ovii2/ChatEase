6169da15c31741f3053c2b4ab07e67412c737999
Ovidijus <ovidijus.e@gmail.com>
2026-05-09 09:01:12 +0300

    update header to customizable

--- app/src/main/java/com/example/chatease/presentation/screens/login/components/LoginScreenHeader.kt
+++ app/src/main/java/com/example/chatease/presentation/screens/login/components/LoginScreenHeader.kt
@@ -1,9 +1,11 @@
 package com.example.chatease.presentation.screens.login.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,30 +14,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.chatease.R
 import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 
 @Composable
-fun LoginScreenHeader(modifier: Modifier = Modifier) {
+fun LoginScreenHeader(
+    modifier: Modifier = Modifier,
+    logoSize: Dp = 80.dp,
+    titleStyle: TextStyle = MaterialTheme.typography.displaySmall,
+    @StringRes titleText: Int,
+    @StringRes labelText: Int
+) {
     Column(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Image(
+            modifier = Modifier.size(logoSize),
             painter = painterResource(R.drawable.ic_app_logo),
             contentDescription = stringResource(R.string.app_name),
             contentScale = ContentScale.Crop
         )
         Text(
-            text = stringResource(R.string.app_name),
-            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.SemiBold)
+            text = stringResource(titleText),
+            style = titleStyle.copy(fontWeight = FontWeight.SemiBold)
         )
         Text(
-            text = stringResource(R.string.app_moto),
+            text = stringResource(labelText),
             style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
@@ -46,6 +57,9 @@ fun LoginScreenHeader(modifier: Modifier = Modifier) {
 @Composable
 private fun LoginScreenHeaderPreview() {
     ChatEaseTheme() {
-        LoginScreenHeader()
+        LoginScreenHeader(
+            titleText = R.string.app_name,
+            labelText = R.string.app_moto
+        )
     }
 }
