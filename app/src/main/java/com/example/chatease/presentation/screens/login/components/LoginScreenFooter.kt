d8f9181bd4e189c8f0f4ac7d91fa000247ea2f5f
Ovidijus <ovidijus.e@gmail.com>
2026-05-10 08:27:22 +0300

    refactor login screen footer buttons

--- app/src/main/java/com/example/chatease/presentation/screens/login/components/LoginScreenFooter.kt
+++ app/src/main/java/com/example/chatease/presentation/screens/login/components/LoginScreenFooter.kt
@@ -1,7 +1,5 @@
 package com.example.chatease.presentation.screens.login.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,18 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,80 +48,36 @@ fun LoginScreenFooter(modifier: Modifier = Modifier) {
             )
         }
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            IconButton(
-                modifier = Modifier
-                    .size(70.dp)
-                    .border(
-                        width = 0.5.dp,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.3f),
-                        shape = CircleShape
-                    ),
+            LoginScreenFooterButton(
                 onClick = {},
-                colors = IconButtonDefaults.iconButtonColors(
-                    containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.3f)
-                )
-            ) {
-                Image(
-                    modifier = Modifier.size(40.dp),
-                    painter = painterResource(R.drawable.ic_google),
-                    contentDescription = null
-                )
-            }
-            IconButton(
-                modifier = Modifier
-                    .size(70.dp)
-                    .border(
-                        width = 0.5.dp,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.3f),
-                        shape = CircleShape
-                    ),
-                onClick = {},
-                colors = IconButtonDefaults.iconButtonColors(
-                    containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.3f)
-                )
-            ) {
-                Image(
-                    modifier = Modifier.size(40.dp),
-                    painter = painterResource(R.drawable.ic_facebook),
-                    contentDescription = null
-                )
-            }
-            IconButton(
-                modifier = Modifier
-                    .size(70.dp)
-                    .border(
-                        width = 0.5.dp,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.3f),
-                        shape = CircleShape
-                    ),
-                onClick = {},
-                colors = IconButtonDefaults.iconButtonColors(
-                    containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.3f)
-                )
-            ) {
-                Image(
-                    modifier = Modifier.size(40.dp),
-                    painter = painterResource(appleImage),
-                    contentDescription = null
-                )
-            }
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(R.string.dont_have_an_account),
-                style = MaterialTheme.typography.labelLarge,
-                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f)
+                icon = R.drawable.ic_google
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = stringResource(R.string.sign_up),
-                style = MaterialTheme.typography.labelLarge,
-                color = MaterialTheme.colorScheme.primary
+            LoginScreenFooterButton(
+                onClick = {},
+                icon = R.drawable.ic_facebook
             )
+            LoginScreenFooterButton(
+                onClick = {},
+                icon = appleImage
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.dont_have_an_account),
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = stringResource(R.string.sign_up),
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     }
 }
