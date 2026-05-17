bf5750d4f941f2550caf8fa58cbd853280c60d05
Ovidijus <ovidijus.e@gmail.com>
2026-05-17 17:43:06 +0300

    add message list

--- app/src/main/java/com/example/chatease/presentation/screens/chats/components/panes/right_pane/RightPane.kt
+++ app/src/main/java/com/example/chatease/presentation/screens/chats/components/panes/right_pane/RightPane.kt
@@ -1,10 +1,12 @@
 package com.example.chatease.presentation.screens.chats.components.panes.right_pane
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chatease.domain.model.Message
 import com.example.chatease.domain.model.User
 import com.example.chatease.domain.model.UserStatus
 import com.example.chatease.presentation.ui.theme.ChatEaseTheme
@@ -12,10 +14,20 @@ import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 @Composable
 fun RightPane(
     modifier: Modifier = Modifier,
-    user: User
+    user: User,
+    messages: List<Message>,
+    currentUserId: String
 ) {
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
         RightPaneTopBar(
+            user = user
+        )
+        MessagesList(
+            messages = messages,
+            currentUserId = currentUserId,
             user = user
         )
     }
@@ -31,10 +43,55 @@ private fun RightPanePreview() {
         imageUrl = null,
         status = UserStatus.ONLINE
     )
+
+    val messages = listOf(
+        Message(
+            messageId = "1",
+            conversationId = "conversation_1",
+            senderId = "user_1",
+            text = "Hey! Are we still for lunch?",
+            timeStamp = System.currentTimeMillis(),
+            seenBy = listOf("user_2")
+        ),
+        Message(
+            messageId = "2",
+            conversationId = "conversation_1",
+            senderId = "user_2",
+            text = "I was thinking maybe we could also stop by that new café near the park afterwards. I heard they have really good desserts and coffee there 😄",
+            timeStamp = System.currentTimeMillis(),
+            seenBy = listOf("user_1")
+        ),
+        Message(
+            messageId = "3",
+            conversationId = "conversation_1",
+            senderId = "user_1",
+            text = "Yes, definitely! GG 😄",
+            timeStamp = System.currentTimeMillis(),
+            seenBy = listOf("user_1")
+        ),
+        Message(
+            messageId = "4",
+            conversationId = "conversation_1",
+            senderId = "user_1",
+            text = "Yes, definitely! 😄",
+            timeStamp = System.currentTimeMillis(),
+            seenBy = listOf("user_1")
+        ),
+        Message(
+            messageId = "5",
+            conversationId = "conversation_1",
+            senderId = "user_2",
+            text = "Yes, definitely! 😄",
+            timeStamp = System.currentTimeMillis(),
+            seenBy = listOf("user_1")
+        )
+    )
     ChatEaseTheme() {
         Column() {
             RightPane(
-                user = user
+                user = user,
+                messages = messages,
+                currentUserId = "user_2",
             )
         }
     }
