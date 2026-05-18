db082c828e3398d3c203901af4bdf46557494fee
Ovidijus <ovidijus.e@gmail.com>
2026-05-18 17:52:39 +0300

    add dependencies

--- app/src/main/java/com/example/chatease/di/AppModule.kt
+++ app/src/main/java/com/example/chatease/di/AppModule.kt
@@ -2,7 +2,11 @@ package com.example.chatease.di
 
 import com.example.chatease.data.remote.CategoryRemoteDataSource
 import com.example.chatease.data.repository.CategoryRepositoryImpl
+import com.example.chatease.data.repository.ConversationRepositoryImpl
+import com.example.chatease.data.repository.UserRepositoryImpl
 import com.example.chatease.domain.repository.CategoryRepository
+import com.example.chatease.domain.repository.ConversationRepository
+import com.example.chatease.domain.repository.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
@@ -32,5 +36,15 @@ object AppModule {
     @Singleton
     fun provideCategoryRepository(remoteDataSource: CategoryRemoteDataSource): CategoryRepository =
         CategoryRepositoryImpl(remoteDataSource)
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(firestore: FirebaseFirestore): UserRepository =
+        UserRepositoryImpl(firestore)
+
+    @Provides
+    @Singleton
+    fun provideConversationRepository(firestore: FirebaseFirestore): ConversationRepository =
+        ConversationRepositoryImpl(firestore)
 
 }
