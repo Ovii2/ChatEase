96b9998f8be672978e73d601f5c325f445369115
Ovidijus <ovidijus.e@gmail.com>
2026-05-18 18:48:38 +0300

    update home ui state

--- app/src/main/java/com/example/chatease/presentation/ui/state/HomeUiState.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/state/HomeUiState.kt
@@ -4,10 +4,16 @@ import com.example.chatease.domain.model.Category
 import com.example.chatease.domain.model.Conversation
 import com.example.chatease.domain.model.User
 
-data class HomeUiState(
-    val isLoading: Boolean = false,
-    val user: User = User(),
-    val categories: List<Category> = emptyList(),
-    val conversations: List<Conversation> = emptyList(),
-    val errorMessage: String? = null
-)
+sealed class HomeUiState {
+    
+    object Loading : HomeUiState()
+    data class Success(
+        val user: User = User(),
+        val categories: List<Category> = emptyList(),
+        val conversations: List<Conversation> = emptyList()
+    ) : HomeUiState()
+
+    data class Error(
+        val message: String
+    ) : HomeUiState()
+}
