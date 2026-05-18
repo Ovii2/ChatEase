c66896a318cf77c2fe9da16192bb7c5c9eb3e99f
Ovidijus <ovidijus.e@gmail.com>
2026-05-18 08:20:30 +0300

    update chat screen

--- app/src/main/java/com/example/chatease/presentation/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/navigation/Screens.kt
@@ -5,8 +5,12 @@ sealed class Screens(val route: String) {
     data object Login : Screens("login")
     data object SignUp : Screens("sign_up")
     data object Home : Screens("home")
-    
-    data object Chat : Screens("chat")
+    data object Chat : Screens("chat/{conversationId") {
+        fun createRoute(conversationId: String): String {
+            return "chat/$conversationId"
+        }
+    }
+
     data object Contacts : Screens("contacts")
     data object Calls : Screens("calls")
     data object Profile : Screens("profile")
