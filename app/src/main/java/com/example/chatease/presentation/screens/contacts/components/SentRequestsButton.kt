02de8574ca7dfc5fc8c0597741dc405dd06582a7
Ovidijus <ovidijus.e@gmail.com>
2026-05-19 19:36:17 +0300

    add sent requests button

---
+++ app/src/main/java/com/example/chatease/presentation/screens/contacts/components/SentRequestsButton.kt
@@ -0,0 +1,82 @@
+package com.example.chatease.presentation.screens.contacts.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.chatease.R
+
+@Composable
+fun SentRequestsButton(
+    modifier: Modifier = Modifier,
+    onNavigateToSentRequests: () -> Unit,
+    count: Int
+) {
+    Button(
+        modifier = modifier,
+        onClick = onNavigateToSentRequests,
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.25f),
+            contentColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .offset(y = (-2).dp)
+                        .size(20.dp)
+                        .rotate(-45f),
+                    imageVector = Icons.AutoMirrored.Outlined.Send,
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.sent_requests),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Row() {
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(color = MaterialTheme.colorScheme.primary)
+                        .size(24.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "$count",
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+        }
+    }
+}
