fd0dd72155c5eab983a1da5d699b4f0aac015be1
Ovidijus <ovidijus.e@gmail.com>
2026-05-19 19:36:55 +0300

    add pending requests button

---
+++ app/src/main/java/com/example/chatease/presentation/screens/contacts/components/PendingItemRequestButton.kt
@@ -0,0 +1,43 @@
+package com.example.chatease.presentation.screens.contacts.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PendingItemRequestButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    icon: ImageVector,
+    containerColor: Color,
+    iconColor: Color
+) {
+    Box(
+        modifier = modifier
+            .size(40.dp)
+            .clip(CircleShape)
+    ) {
+        IconButton(
+            onClick = onClick,
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = containerColor
+            )
+        ) {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = icon,
+                contentDescription = null,
+                tint = iconColor
+            )
+        }
+    }
+}
