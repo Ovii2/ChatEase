5e7b64301f26e21b0cc79116ffb766d88b5abf82
Ovidijus <ovidijus.e@gmail.com>
2026-05-27 17:41:52 +0300

    update view model

--- app/src/main/java/com/example/chatease/presentation/ui/viewmodel/AllRequestsViewModel.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/viewmodel/AllRequestsViewModel.kt
@@ -3,12 +3,12 @@ package com.example.chatease.presentation.ui.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.chatease.R
 import com.example.chatease.domain.repository.ContactRequestRepository
 import com.example.chatease.domain.repository.UserRepository
 import com.example.chatease.presentation.ui.model.PendingRequestUiModel
 import com.example.chatease.presentation.ui.model.SentRequestUiModel
 import com.example.chatease.presentation.ui.state.ReceivedRequestsUiState
+import com.example.chatease.presentation.ui.state.SentRequestsUiState
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -28,57 +28,97 @@ class AllRequestsViewModel @Inject constructor(
     )
     val receivedRequests = _receivedRequests.asStateFlow()
 
-    private val _sentRequests = MutableStateFlow<List<SentRequestUiModel>>(emptyList())
+    private val _sentRequests = MutableStateFlow<SentRequestsUiState>(SentRequestsUiState.Loading)
     val sentRequests = _sentRequests.asStateFlow()
 
     init {
-        loadReceivedRequests()
-        loadSentRequests()
+        observeReceivedRequests()
+        observeSentRequests()
     }
 
-    fun loadReceivedRequests() {
+    fun acceptContactRequest(requestId: String) {
         viewModelScope.launch {
             try {
-                val currentUserId = auth.currentUser?.uid ?: return@launch
-                val receivedRequests = contactRequestRepository.getPendingRequests(currentUserId)
-                val pendingRequestUiModels = receivedRequests.map { request ->
-                    PendingRequestUiModel(
-                        requestId = request.id,
-                        user = userRepository.getUserById(request.senderUserId)
-                    )
-                }
-                _receivedRequests.value = if (pendingRequestUiModels.isEmpty()) {
-                    ReceivedRequestsUiState.Empty
-                } else {
-                    ReceivedRequestsUiState.Success(
-                        requests = pendingRequestUiModels
-                    )
-                }
+                contactRequestRepository.acceptContactRequest(requestId)
             } catch (e: Exception) {
-                ReceivedRequestsUiState.Error(
-                    errorMessage = R.string.fail_load_received_requests
+                Log.e("AllRequestsViewModel", e.message ?: "Failed to accept request")
+            }
+        }
+    }
+
+    fun declineContactRequest(requestId: String) {
+        viewModelScope.launch {
+            try {
+                contactRequestRepository.declineContactRequest(requestId)
+            } catch (e: Exception) {
+                Log.e("AllRequestsViewModel", e.message ?: "Failed to decline request")
+            }
+        }
+    }
+
+    fun withDrawContactRequest(request: SentRequestUiModel) {
+        viewModelScope.launch {
+            try {
+                val senderUserId = auth.currentUser?.uid ?: return@launch
+
+                contactRequestRepository.withdrawContactRequest(
+                    requestId = request.requestId,
+                    senderUserId = senderUserId,
+                    receiverUserId = request.receiver.uid
                 )
-                Log.e("AllRequestsViewModel", e.message ?: "Failed to load received requests")
+            } catch (e: Exception) {
+                Log.e("AllRequestsViewModel", e.message ?: "Failed to withdraw request")
             }
         }
     }
 
-    fun loadSentRequests() {
+    private fun observeReceivedRequests() {
         viewModelScope.launch {
-            try {
-                val currentUserId = auth.currentUser?.uid ?: return@launch
-                val sentRequests = contactRequestRepository.getSentRequests(currentUserId)
-                val sentRequestsUiModels = sentRequests.map { request ->
-                    SentRequestUiModel(
-                        requestId = request.id,
-                        receiver = userRepository.getUserById(request.receiverUserId),
-                        status = request.status
-                    )
+            val currentUserId = auth.currentUser?.uid ?: return@launch
+
+            contactRequestRepository
+                .observePendingRequests(currentUserId)
+                .collect { requests ->
+                    val uiModels = requests.map { request ->
+                        PendingRequestUiModel(
+                            requestId = request.id,
+                            user = userRepository.getUserById(request.senderUserId)
+                        )
+                    }
+
+                    _receivedRequests.value =
+                        if (uiModels.isEmpty()) {
+                            ReceivedRequestsUiState.Empty
+                        } else {
+                            ReceivedRequestsUiState.Success(uiModels)
+                        }
                 }
-                _sentRequests.value = sentRequestsUiModels
-            } catch (e: Exception) {
-                Log.e("AllRequestsViewModel", e.message ?: "Failed to load sent requests")
-            }
         }
     }
+
+    private fun observeSentRequests() {
+        viewModelScope.launch {
+            val currentUserId = auth.currentUser?.uid ?: return@launch
+
+            contactRequestRepository
+                .observeSentRequests(currentUserId)
+                .collect { requests ->
+                    val uiModels = requests.map { request ->
+                        SentRequestUiModel(
+                            requestId = request.id,
+                            receiver = userRepository.getUserById(request.receiverUserId),
+                            status = request.status
+                        )
+                    }
+
+                    _sentRequests.value =
+                        if (uiModels.isEmpty()) {
+                            SentRequestsUiState.Empty
+                        } else {
+                            SentRequestsUiState.Success(uiModels)
+                        }
+                }
+        }
+    }
+
 }
