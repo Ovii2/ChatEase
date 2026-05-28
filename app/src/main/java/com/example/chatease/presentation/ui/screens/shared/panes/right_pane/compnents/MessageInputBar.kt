d6412c036b2c367c1798797b5ebf8a5c48bbcaf6
Ovidijus <ovidijus.e@gmail.com>
2026-05-28 17:48:22 +0300

    add capitalize first letter of sentence

--- app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/right_pane/compnents/MessageInputBar.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/right_pane/compnents/MessageInputBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
@@ -27,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.chatease.R
@@ -79,7 +81,8 @@ fun MessageInputBar(
                     imageVector = Icons.Outlined.MoreVert, contentDescription = null
                 )
             }
-        }
+        },
+        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
     )
 }
 
