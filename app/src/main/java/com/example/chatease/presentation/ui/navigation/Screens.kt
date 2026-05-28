8b786bff6dba41ab945e35b19f615a4671d2c664
Ovidijus <ovidijus.e@gmail.com>
2026-05-28 17:48:38 +0300

    add curly brace

--- app/src/main/java/com/example/chatease/presentation/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/navigation/Screens.kt
@@ -8,7 +8,7 @@ sealed class Screens(val route: String) {
     data object Login : Screens("login")
     data object SignUp : Screens("sign_up")
     data object Home : Screens("home")
-    data object Chat : Screens("chat/{conversationId") {
+    data object Chat : Screens("chat/{conversationId}") {
         fun createRoute(conversationId: String): String {
             return "chat/$conversationId"
         }
