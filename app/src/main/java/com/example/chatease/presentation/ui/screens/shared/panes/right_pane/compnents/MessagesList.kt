3f11275547307a18de5e44654bd4a4176ed146e6
Ovidijus <ovidijus.e@gmail.com>
2026-05-31 17:45:39 +0300

    add state

--- app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/right_pane/compnents/MessagesList.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/right_pane/compnents/MessagesList.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,15 +27,16 @@ fun MessagesList(
     modifier: Modifier = Modifier,
     messages: List<Message>,
     currentUserId: String,
-    user: User
+    user: User,
+    listState: LazyListState
 ) {
     val firstUnreadMessageId = "3"
 
     LazyColumn(
         modifier = modifier,
+        state = listState,
         verticalArrangement = Arrangement.spacedBy(18.dp)
     ) {
-
         items(messages) { message ->
             val isSentByCurrentUser = message.senderId == currentUserId
             if (message.messageId == firstUnreadMessageId) {
@@ -116,6 +120,7 @@ private fun MessagesListPreview() {
                 messages = messages,
                 currentUserId = "user_2",
                 user = user,
+                listState = rememberLazyListState(),
             )
         }
     }
