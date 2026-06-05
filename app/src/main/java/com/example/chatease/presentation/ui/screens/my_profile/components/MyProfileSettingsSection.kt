257d28b1190879526c165b4681a76d781764c0d0
Ovidijus <ovidijus.e@gmail.com>
2026-06-05 10:42:56 +0300

    add my profile settings section

---
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/my_profile/components/MyProfileSettingsSection.kt
@@ -0,0 +1,148 @@
+package com.example.chatease.presentation.ui.screens.my_profile.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.automirrored.outlined.ContactSupport
+import androidx.compose.material.icons.outlined.ContactSupport
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chatease.R
+import com.example.chatease.presentation.ui.theme.ChatEaseTheme
+
+@Composable
+fun MyProfileSettingsSection(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(3.dp)
+    ) {
+        MyProfileSettingsItem(
+            onClick = {},
+            imageVector = Icons.Outlined.Edit,
+            text = R.string.edit_profile
+        )
+        MyProfileSettingsItem(
+            onClick = {},
+            imageVector = Icons.Outlined.Person,
+            text = R.string.account_settings
+        )
+        MyProfileSettingsItem(
+            onClick = {},
+            imageVector = Icons.Outlined.Lock,
+            text = R.string.privacy_security
+        )
+        MyProfileSettingsItem(
+            onClick = {},
+            imageVector = Icons.Outlined.Notifications,
+            text = R.string.notifications
+        )
+        MyProfileSettingsItem(
+            onClick = {},
+            imageVector = Icons.AutoMirrored.Outlined.ContactSupport,
+            text = R.string.help_support
+        )
+    }
+}
+
+@Composable
+fun MyProfileSettingsItem(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    imageVector: ImageVector,
+    @StringRes text: Int
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .clip(RoundedCornerShape(15.dp))
+            .height(60.dp)
+            .background(color = MaterialTheme.colorScheme.surfaceContainer),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(color = MaterialTheme.colorScheme.primary.copy(alpha = 0.1f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = imageVector,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+                Text(
+                    text = stringResource(text),
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.W500
+                )
+            }
+            Icon(
+                modifier = Modifier.size(20.dp),
+                imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun MyProfileSettingsSectionPreview() {
+    ChatEaseTheme {
+        Scaffold { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MyProfileSettingsSection()
+            }
+        }
+    }
+}
