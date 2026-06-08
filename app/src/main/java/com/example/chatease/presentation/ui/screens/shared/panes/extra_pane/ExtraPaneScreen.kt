1389b13fb926ef2286b23d4fb1454ded2b710856
Ovidijus <ovidijus.e@gmail.com>
2026-06-08 17:31:41 +0300

    update extra pane screen

--- app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/extra_pane/ExtraPaneScreen.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/extra_pane/ExtraPaneScreen.kt
@@ -11,18 +11,30 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.chatease.domain.model.MediaItem
 import com.example.chatease.domain.model.User
+import com.example.chatease.domain.model.enums.MediaType
 import com.example.chatease.domain.model.enums.UserPresenceStatus
 import com.example.chatease.presentation.ui.screens.shared.panes.extra_pane.components.about_section.ExtraPaneAboutSection
+import com.example.chatease.presentation.ui.screens.shared.panes.extra_pane.components.media_section.ExtraPaneMediaSection
+import com.example.chatease.presentation.ui.screens.shared.panes.extra_pane.components.more_section.ExtraPaneMoreSection
+import com.example.chatease.presentation.ui.screens.shared.panes.extra_pane.components.notifications_section.ExtraPaneNotificationsSection
 import com.example.chatease.presentation.ui.screens.shared.panes.extra_pane.components.top_section.ExtraPaneTopSection
 import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 
 @Composable
 fun ExtraPaneScreen(modifier: Modifier = Modifier) {
+    var checked by rememberSaveable { mutableStateOf(false) }
+    val iconSize = 26.dp
+
     val user = User(
         uid = "",
         fullName = "Test Test",
@@ -30,6 +42,15 @@ fun ExtraPaneScreen(modifier: Modifier = Modifier) {
         imageUrl = null,
         status = UserPresenceStatus.ONLINE
     )
+
+    val mediaItems = List(2) { index ->
+        MediaItem(
+            id = index.toString(),
+            thumbnailUrl = "https://picsum.photos/200/200?random=$index",
+            mediaUrl = "https://picsum.photos/1200/1200?random=$index",
+            type = MediaType.IMAGE
+        )
+    }
     Scaffold { paddingValues ->
         Box(
             modifier = Modifier.fillMaxSize(),
@@ -48,6 +69,23 @@ fun ExtraPaneScreen(modifier: Modifier = Modifier) {
                     user = user
                 )
                 ExtraPaneAboutSection()
+                if (mediaItems.isNotEmpty()) {
+                    ExtraPaneMediaSection(
+                        items = mediaItems
+                    )
+                }
+                ExtraPaneNotificationsSection(
+                    checked = checked,
+                    onCheckedChange = { checked = it },
+                    iconSize = iconSize,
+                )
+                ExtraPaneMoreSection(
+                    iconSize = iconSize,
+                    onViewContactClick = {},
+                    onShareContactClick = {},
+                    onBlockContactClick = {},
+                    onDeleteContactClick = {}
+                )
             }
         }
     }
