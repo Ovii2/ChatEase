a6185b71722667669eaab7a5bd68107fe4b109df
Ovidijus <ovidijus.e@gmail.com>
2026-06-12 08:26:21 +0300

    update other user screen

--- app/src/main/java/com/example/chatease/presentation/ui/screens/other_user_profile/OtherUserProfileScreen.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/other_user_profile/OtherUserProfileScreen.kt
@@ -2,69 +2,78 @@ package com.example.chatease.presentation.ui.screens.other_user_profile
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.chatease.R
-import com.example.chatease.domain.model.User
-import com.example.chatease.domain.model.enums.UserPresenceStatus
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.chatease.presentation.ui.screens.other_user_profile.components.OtherUserProfileAboutSection
 import com.example.chatease.presentation.ui.screens.other_user_profile.components.OtherUserProfileActionsSection
 import com.example.chatease.presentation.ui.screens.other_user_profile.components.OtherUserProfileTopSection
 import com.example.chatease.presentation.ui.screens.shared.chat.CommonTopBar
 import com.example.chatease.presentation.ui.theme.ChatEaseTheme
+import com.example.chatease.presentation.ui.viewmodel.OtherUserProfileViewModel
 
 @Composable
 fun OtherUserProfileScreen(
     modifier: Modifier = Modifier,
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+    otherUserProfileViewModel: OtherUserProfileViewModel = hiltViewModel(),
+    userId: String
 ) {
-    val user = User(
-        uid = "",
-        fullName = "Test Test",
-        email = "test@email.com",
-        imageUrl = null,
-        status = UserPresenceStatus.ONLINE
-    )
-    val isConnected = false
+    val user by otherUserProfileViewModel.user.collectAsState()
+    val isConnected by otherUserProfileViewModel.isUserConnected.collectAsState()
 
-    Scaffold(topBar = {
-        CommonTopBar(
-            onBackClick = onBackClick,
-        )
-    }) { paddingValues ->
-        Column(
-            modifier = modifier
+    LaunchedEffect(userId) {
+        otherUserProfileViewModel.loadUser(userId)
+        otherUserProfileViewModel.checkIfUserConnected(userId)
+    }
+
+    Scaffold(
+        modifier = modifier.padding(vertical = 8.dp, horizontal = 12.dp),
+        topBar = {
+            CommonTopBar(
+                onBackClick = onBackClick,
+            )
+        }) { paddingValues ->
+        Box(
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
-                .padding(vertical = 8.dp, horizontal = 12.dp),
-            verticalArrangement = Arrangement.spacedBy(32.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(paddingValues),
+            contentAlignment = Alignment.TopCenter
         ) {
-            OtherUserProfileTopSection(
-                user = user,
-                isConnected = isConnected,
-                onSendRequest = {},
-                onSendMessage = {},
-            )
-            OtherUserProfileActionsSection(
-                onViewPhotoClick = {},
-                onBlockClick = {},
-                onReportClick = {}
-            )
-            OtherUserProfileAboutSection()
+            Column(
+                modifier = modifier
+                    .widthIn(max = 600.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OtherUserProfileTopSection(
+                    user = user,
+                    isConnected = isConnected,
+                    onSendRequest = {},
+                    onSendMessage = {},
+                )
+                OtherUserProfileActionsSection(
+                    onViewPhotoClick = {},
+                    onBlockClick = {},
+                    onReportClick = {}
+                )
+                OtherUserProfileAboutSection()
+            }
         }
     }
 }
@@ -84,7 +93,8 @@ private fun OtherUserProfileScreenPreview() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 OtherUserProfileScreen(
-                    onBackClick = {}
+                    onBackClick = {},
+                    userId = "",
                 )
             }
         }
