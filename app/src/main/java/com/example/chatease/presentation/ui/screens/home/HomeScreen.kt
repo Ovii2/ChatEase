9368f29fa84fd7db0561a4b8ce5071bebcef8253
Ovidijus <ovidijus.e@gmail.com>
2026-06-15 16:39:55 +0300

    add progress bar

--- app/src/main/java/com/example/chatease/presentation/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/home/HomeScreen.kt
@@ -4,7 +4,9 @@ import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.NavigationDrawerItemDefaults
@@ -31,9 +33,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.chatease.R
 import com.example.chatease.domain.model.Category
 import com.example.chatease.domain.model.User
 import com.example.chatease.domain.model.enums.UserPresenceStatus
@@ -186,7 +190,8 @@ fun HomeScreen(
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(text = "Loading")
+                        LinearProgressIndicator(modifier = Modifier.height(8.dp))
+                        Text(text = stringResource(R.string.loading))
                     }
                 }
 
