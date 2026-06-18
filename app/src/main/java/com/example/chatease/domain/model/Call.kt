ed7ebfe673be7d49d66d20032e1925f9d74b378b
Ovidijus <ovidijus.e@gmail.com>
2026-06-18 08:06:59 +0300

    update call model

--- app/src/main/java/com/example/chatease/domain/model/Call.kt
+++ app/src/main/java/com/example/chatease/domain/model/Call.kt
@@ -4,9 +4,9 @@ import com.example.chatease.domain.model.enums.CallStatus
 import com.example.chatease.domain.model.enums.CallType
 
 data class Call(
-    val id: String,
-    val callerId: String,
-    val receiverId: String,
-    val callType: CallType,
-    val status: CallStatus
+    val id: String = "",
+    val callerId: String = "",
+    val receiverId: String = "",
+    val callType: CallType = CallType.AUDIO,
+    val status: CallStatus = CallStatus.CALLING
 )
