527326853adf0ae271b56d66310881b55a1f2d02
Ovidijus <ovidijus.e@gmail.com>
2026-06-21 19:08:38 +0300

    align content, add padding, scroll

--- app/src/main/java/com/example/chatease/presentation/ui/screens/shared/calls/ActiveCallScreenLayout.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/calls/ActiveCallScreenLayout.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -67,7 +70,8 @@ fun ActiveCallScreenLayout(
                 startY = offset,
                 endY = offset + 1000f
             )
-        )
+        ),
+        contentAlignment = Alignment.TopCenter
     ) {
         Scaffold(
             modifier = Modifier
@@ -79,9 +83,13 @@ fun ActiveCallScreenLayout(
         ) { paddingValues ->
             Column(
                 modifier = Modifier
+                    .fillMaxSize()
                     .padding(paddingValues)
+                    .padding(vertical = 8.dp)
+                    .verticalScroll(rememberScrollState())
                     .widthIn(max = 600.dp),
-                verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterVertically)
+                verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 content()
             }
