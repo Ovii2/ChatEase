ebabe2210a03c64e82af806193059df9c9850e94
Ovidijus <ovidijus.e@gmail.com>
2026-06-22 09:23:39 +0300

    remove data source

--- app/src/main/java/com/example/chatease/domain/model/AudioCallActionItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chatease.domain.model
-
-import androidx.annotation.StringRes
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class AudioCallActionItem(
-    @StringRes val label: Int,
-    val icon: ImageVector
-)
