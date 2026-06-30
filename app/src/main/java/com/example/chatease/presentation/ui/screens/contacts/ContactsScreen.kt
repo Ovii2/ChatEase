769a99be7c07ed11431821c311f4d6c47b98ea7c
Ovidijus <ovidijus.e@gmail.com>
2026-06-30 10:59:39 +0300

    add animation when there are no contacts

--- app/src/main/java/com/example/chatease/presentation/ui/screens/contacts/ContactsScreen.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/contacts/ContactsScreen.kt
@@ -1,7 +1,8 @@
 package com.example.chatease.presentation.ui.screens.contacts
 
-import android.annotation.SuppressLint
-import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -11,15 +12,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PersonAdd
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.chatease.R
@@ -31,7 +38,6 @@ import com.example.chatease.presentation.ui.screens.contacts.components.MyContac
 import com.example.chatease.presentation.ui.screens.contacts.components.PendingRequestsSection
 import com.example.chatease.presentation.ui.screens.contacts.components.SentRequestsButton
 import com.example.chatease.presentation.ui.screens.shared.chat.ChatSearchBar
-import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 import com.example.chatease.presentation.ui.viewmodel.ContactsViewModel
 
 @Composable
@@ -53,10 +59,12 @@ fun ContactsScreen(
     val pendingRequestLimit = 3
     val receivedRequestUserIds by contactsViewModel.receivedRequestUserIds.collectAsState()
     val contacts by contactsViewModel.contacts.collectAsState()
+    var isVisible by rememberSaveable { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         contactsViewModel.getSentRequests()
         contactsViewModel.getContacts()
+        isVisible = true
     }
 
     Scaffold(
@@ -131,27 +139,38 @@ fun ContactsScreen(
                         contacts = contacts,
                         onContactClick = { onNavigateToOtherUserProfile(it) }
                     )
+                } else {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        AnimatedVisibility(
+                            visible = isVisible,
+                            enter = slideInVertically(
+                                animationSpec = tween(500),
+                                initialOffsetY = { -it })
+                        ) {
+                            Text(
+                                text = stringResource(R.string.no_contacts),
+                                style = MaterialTheme.typography.headlineLarge
+                            )
+                        }
+                        AnimatedVisibility(
+                            visible = isVisible,
+                            enter = slideInVertically(
+                                animationSpec = tween(500, delayMillis = 250),
+                                initialOffsetY = { -it })
+                        ) {
+                            Text(
+                                text = stringResource(R.string.all_contacts_shown_here),
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
                 }
             }
-        }
-    }
-}
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Preview(
-    showBackground = true, showSystemUi = true,
-    uiMode = Configuration.UI_MODE_TYPE_NORMAL
-)
-@Composable
-private fun ContactsScreenPreview(contactsViewModel: ContactsViewModel = hiltViewModel()) {
-    ChatEaseTheme {
-        Scaffold {
-            ContactsScreen(
-                onNavigateToSentRequests = {},
-                onBackClick = {},
-                onNavigateToAllRequests = {},
-                onNavigateToOtherUserProfile = {},
-            )
         }
     }
 }
