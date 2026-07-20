2637d1bbfe384e00e2b92ed8ca5857b006a6e01f
Ovidijus <ovidijus.e@gmail.com>
2026-07-20 20:51:01 +0300

    add common error display

---
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/error/CommonErrorDisplay.kt
@@ -0,0 +1,36 @@
+package com.example.chatease.presentation.ui.screens.shared.error
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun CommonErrorDisplay(
+    modifier: Modifier = Modifier,
+    @StringRes errorText: Int
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Warning,
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(errorText),
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+}
