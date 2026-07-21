0137717b72c49efde9297863d872fa11f4e17f6c
Ovidijus <ovidijus.e@gmail.com>
2026-07-21 09:43:13 +0300

    update error display

--- app/src/main/java/com/example/chatease/presentation/ui/screens/shared/error/CommonErrorDisplay.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/error/CommonErrorDisplay.kt
@@ -1,36 +1,59 @@
 package com.example.chatease.presentation.ui.screens.shared.error
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chatease.R
+import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 
 @Composable
 fun CommonErrorDisplay(
-    modifier: Modifier = Modifier,
-    @StringRes errorText: Int
+    modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
+            modifier = Modifier.size(38.dp),
             imageVector = Icons.Filled.Warning,
-            contentDescription = null
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.error
         )
         Text(
-            text = stringResource(errorText),
+            text = stringResource(R.string.error_text),
             style = MaterialTheme.typography.titleMedium
         )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun CommonErrorDisplayPreview() {
+    ChatEaseTheme {
+        Scaffold { paddingValues ->
+            Column(
+                modifier = Modifier.padding(paddingValues),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CommonErrorDisplay()
+            }
+        }
     }
 }
