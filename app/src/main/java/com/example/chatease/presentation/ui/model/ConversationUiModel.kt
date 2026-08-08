55207306615acbf4799bb03fe4fd7c021b250ea6
Ovidijus <ovidijus.e@gmail.com>
2026-08-08 07:17:49 +0300

    add fields

--- app/src/main/java/com/example/chatease/presentation/ui/model/ConversationUiModel.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/model/ConversationUiModel.kt
@@ -1,6 +1,7 @@
 package com.example.chatease.presentation.ui.model
 
 import com.example.chatease.domain.model.User
+import com.example.chatease.domain.model.enums.MessageType
 
 data class ConversationUiModel(
     val conversationId: String,
@@ -8,10 +9,12 @@ data class ConversationUiModel(
     val imageUrl: String?,
     val participants: List<User>,
     val lastMessage: String,
+    val lastMessageType: MessageType,
     val timestamp: Long,
     val unreadCount: Int,
     val isGroup: Boolean,
     val isBlockedByOtherUser: Boolean = false,
     val isCurrentUserGroupMember: Boolean,
-    val categoryId: String? = null
+    val categoryId: String? = null,
+    val lastMessageSenderId: String = ""
 )
