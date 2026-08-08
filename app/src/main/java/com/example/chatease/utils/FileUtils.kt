eeef14ad388976bf8710e785a8320ec01ce76413
Ovidijus <ovidijus.e@gmail.com>
2026-08-08 17:06:32 +0300

    update file utils

--- app/src/main/java/com/example/chatease/utils/FileUtils.kt
+++ app/src/main/java/com/example/chatease/utils/FileUtils.kt
@@ -1,5 +1,9 @@
 package com.example.chatease.utils
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
 fun String.toTruncatedFileName(): String {
     if (this.length >= 35) {
         return this.take(20) + "..." + this.takeLast(12)
@@ -16,4 +20,19 @@ fun Long.toFormattedFileSize(): String {
         this < oneMb -> "%.2f KB".format(this.toDouble() / oneKb)
         else -> "%.2f MB".format(this.toDouble() / oneMb)
     }
+}
+
+fun openFile(
+    context: Context,
+    uri: Uri,
+    mimeType: String
+) {
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+        setDataAndType(uri, mimeType)
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
+
+    context.startActivity(
+        Intent.createChooser(intent, null)
+    )
 }
