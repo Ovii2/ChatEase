bdf551ce7286d6d44aa93074d07e1f91e7f23f45
Ovidijus <ovidijus.e@gmail.com>
2026-08-09 20:01:25 +0300

    extract reply bubble

---
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/shared/panes/right_pane/compnents/ReplyFileBubble.kt
@@ -0,0 +1,126 @@
+package com.example.chatease.presentation.ui.screens.shared.panes.right_pane.compnents
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.chatease.domain.model.FileAttachment
+import com.example.chatease.domain.model.Message
+import com.example.chatease.domain.model.ReplyMessage
+import com.example.chatease.domain.model.enums.MessageType
+import com.example.chatease.presentation.ui.screens.shared.chat.MessageBubbleContainer
+import com.example.chatease.presentation.ui.screens.shared.chat.ReplyBubbleContainer
+import com.example.chatease.presentation.ui.theme.ChatEaseTheme
+
+@Composable
+fun ReplyFileBubble(
+    modifier: Modifier = Modifier,
+    filename: String,
+    isSentByCurrentUser: Boolean,
+    message: Message,
+    showReactions: Boolean,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    onDismissReactions: () -> Unit,
+    onReactionClick: (String, String) -> Unit,
+    onShowUsersReactionsClick: (String) -> Unit,
+    isBlockedByOtherUser: Boolean,
+    isUserMemberOfGroup: Boolean,
+    currentUserId: String,
+    messageSenderName: String,
+    repliedMessageSenderName: String
+) {
+    ReplyBubbleContainer(
+        modifier = modifier.widthIn(max = 300.dp),
+        isSentByCurrentUser = isSentByCurrentUser,
+        message = message,
+        currentUserId = currentUserId,
+        messageSenderName = messageSenderName,
+        repliedMessageSenderName = repliedMessageSenderName,
+        replyPreviewText = filename
+    ) {
+        MessageBubbleContainer(
+            message = message,
+            isSentByCurrentUser = isSentByCurrentUser,
+            showReactions = showReactions,
+            onClick = onClick,
+            onLongClick = onLongClick,
+            onDismissReactions = onDismissReactions,
+            onReactionClick = onReactionClick,
+            onShowUsersReactionsClick = onShowUsersReactionsClick,
+            isBlockedByOtherUser = isBlockedByOtherUser,
+            isUserMemberOfGroup = isUserMemberOfGroup,
+            isReplyMessage = true
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(8.dp),
+                text = message.text,
+                color = MaterialTheme.colorScheme.surface
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ReplyFileBubblePreview() {
+    ChatEaseTheme {
+        Scaffold { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ReplyFileBubble(
+                    isSentByCurrentUser = true,
+                    filename = "test_chat_document_2043434340_23123123.pdf",
+                    message = Message(
+                        messageId = "1",
+                        conversationId = "1",
+                        senderId = "1",
+                        text = "Test message",
+                        timeStamp = System.currentTimeMillis(),
+                        seenBy = listOf("1", "2"),
+                        reactions = emptyMap(),
+                        messageType = MessageType.FILE,
+                        replyMessage = ReplyMessage(
+                            messageId = "1",
+                            senderId = "1",
+                            text = "Replying"
+                        ),
+                        fileAttachment = FileAttachment(
+                            id = "1",
+                            name = "test_chat_document_2043434340_23123123.pdf",
+                            size = 12345L,
+                            url = "",
+                            mimeType = ""
+                        )
+                    ),
+                    currentUserId = "1",
+                    messageSenderName = "Tester",
+                    repliedMessageSenderName = "Test",
+                    showReactions = false,
+                    onClick = {},
+                    onLongClick = {},
+                    onDismissReactions = {},
+                    onReactionClick = { _, _ -> },
+                    onShowUsersReactionsClick = {},
+                    isBlockedByOtherUser = false,
+                    isUserMemberOfGroup = false,
+                )
+            }
+        }
+    }
+}
