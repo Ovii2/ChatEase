8f6819284dc8fb38cc1fe4812e1644d71515a620
Ovidijus <ovidijus.e@gmail.com>
2026-08-13 08:41:59 +0300

    update media and docs screen

--- app/src/main/java/com/example/chatease/presentation/ui/screens/media_and_docs/MediaAndDocsScreen.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/media_and_docs/MediaAndDocsScreen.kt
@@ -1,23 +1,47 @@
 package com.example.chatease.presentation.ui.screens.media_and_docs
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.example.chatease.R
 import com.example.chatease.domain.model.MediaItem
 import com.example.chatease.domain.model.enums.MediaType
 import com.example.chatease.presentation.ui.screens.shared.chat.CommonTopBar
 import com.example.chatease.presentation.ui.theme.ChatEaseTheme
+import com.example.chatease.utils.toFormattedFileSize
 
 @Composable
 fun MediaAndDocsScreen(
@@ -25,13 +49,13 @@ fun MediaAndDocsScreen(
     onBackClick: () -> Unit
 ) {
     var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
-    val mediaItems = List(20) {
+    val mediaItems = List(10) {
         MediaItem(
             id = it.toString(),
             thumbnailUrl = "",
             mediaUrl = "",
-            type = listOf(MediaType.FILE, MediaType.IMAGE).random(),
-            fileName = "file_2123123.pdf",
+            type = MediaType.FILE,
+            fileName = LoremIpsum(10).values.first() + ".pdf",
             fileSize = 123456L,
             mimeType = "",
             senderId = "1",
@@ -39,8 +63,10 @@ fun MediaAndDocsScreen(
             timeStamp = System.currentTimeMillis()
         )
     }
+    var selectedDocId by rememberSaveable { mutableStateOf<String?>(null) }
 
     Scaffold(
+        modifier = modifier.padding(vertical = 12.dp),
         topBar = {
             CommonTopBar(
                 onBackClick = onBackClick,
@@ -49,18 +75,119 @@ fun MediaAndDocsScreen(
         },
         bottomBar = {
             MediaAndDocsTabRow(
+                modifier = Modifier,
                 selectedTabIndex = selectedTabIndex,
                 onMediaClick = { selectedTabIndex = 0 },
                 onDocsClick = { selectedTabIndex = 1 }
             )
         }
     ) { paddingValues ->
-        Text("text")
-    }
 
+        when (selectedTabIndex) {
+            0 -> {
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(minSize = 130.dp)
+                ) {
+                    items(mediaItems) { mediaItem ->
+                        AsyncImage(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(3f / 2f)
+                                .padding(2.dp),
+                            model = mediaItem.mediaUrl,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
+                        )
+                        Image(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(3f / 2f)
+                                .padding(2.dp),
+                            painter = painterResource(R.drawable.person),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+            }
+
+            1 -> {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .padding(horizontal = 8.dp, vertical = 10.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(mediaItems) { mediaItem ->
+                        MediaDocItem(
+                            mediaItem = mediaItem,
+                            onClick = { id ->
+                                selectedDocId = id
+                            },
+                        )
+                    }
+                }
+            }
+        }
+        val selectedDoc = mediaItems.firstOrNull { it.id == selectedDocId }
+
+        selectedDoc?.let { mediaItem ->
+            DocDetailsDialog(
+                onDismiss = { selectedDocId = null },
+                mediaItem = mediaItem,
+                onDownloadClick = {}
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+
+@Composable
+fun MediaDocItem(
+    modifier: Modifier = Modifier,
+    mediaItem: MediaItem,
+    onClick: (String) -> Unit
+) {
+    Surface(
+        modifier = modifier
+            .clickable { onClick(mediaItem.id) }
+            .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.surfaceContainer,
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(
+                vertical = 12.dp,
+                horizontal = 8.dp
+            ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(R.drawable.outline_docs_24),
+                contentDescription = null
+            )
+            Column {
+                Text(
+                    text = mediaItem.fileName,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = mediaItem.fileSize.toFormattedFileSize(),
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true, showSystemUi = true,
+    uiMode = Configuration.UI_MODE_TYPE_NORMAL
+)
 @Composable
 private fun MediaAndDocsScreenPreview() {
     ChatEaseTheme {
