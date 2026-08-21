953df20cece330818f6a00eedc226642cfdd6e46
Ovidijus <ovidijus.e@gmail.com>
2026-08-21 11:17:05 +0300

    add recommended badge

---
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/membership/components/card/MembershipCardRecommendedBadge.kt
@@ -0,0 +1,84 @@
+package com.example.chatease.presentation.ui.screens.membership.components.card
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.StarOutline
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.example.chatease.R
+import com.example.chatease.presentation.ui.theme.ChatEaseTheme
+
+@Composable
+fun MembershipCardRecommendedBadge(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .offset(y = (-15).dp)
+            .size(
+                width = 160.dp,
+                height = 35.dp
+            )
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(15.dp)
+            )
+            .background(
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f),
+                shape = RoundedCornerShape(15.dp)
+            )
+            .zIndex(1f),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.StarOutline,
+                contentDescription = null,
+                tint = Color.White
+            )
+            Text(
+                text = stringResource(R.string.recommended),
+                color = Color.White,
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun MembershipCardRecommendedBadgePreview() {
+    ChatEaseTheme {
+        Scaffold { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MembershipCardRecommendedBadge()
+            }
+        }
+    }
+}
