6152714e3f6201150f6f0a6c556025d1ef9e9f10
Ovidijus <ovidijus.e@gmail.com>
2026-08-21 16:22:10 +0300

    add vertical scroll

--- app/src/main/java/com/example/chatease/presentation/ui/screens/membership/components/MembershipScreenContent.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/membership/components/MembershipScreenContent.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,12 +17,15 @@ import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 @Composable
 fun MembershipScreenContent(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
         MembershipScreenHeader()
         MembershipScreenBody()
+        MembershipScreenFooter()
     }
 }
 
