72aa2117ccc390143b3d69303c7a90c6c06e18c7
Ovidijus <ovidijus.e@gmail.com>
2026-08-21 15:35:41 +0300

    update list position

--- app/src/main/java/com/example/chatease/presentation/ui/screens/membership/components/MembershipScreenBody.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/membership/components/MembershipScreenBody.kt
@@ -1,19 +1,19 @@
 package com.example.chatease.presentation.ui.screens.membership.components
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.chatease.data.local.datasource.MembershipBenefitsDataSource
 import com.example.chatease.domain.model.enums.Membership
 import com.example.chatease.presentation.ui.screens.membership.components.card.MembershipScreenCard
 import com.example.chatease.presentation.ui.theme.ChatEaseTheme
@@ -21,30 +21,45 @@ import com.example.chatease.presentation.ui.theme.ChatEaseTheme
 @Composable
 fun MembershipScreenBody(
     modifier: Modifier = Modifier,
-    isRecommended: Boolean = false
 ) {
-    Row(
-        modifier = modifier.horizontalScroll(rememberScrollState()),
+    val listState = rememberLazyListState()
+    val density = LocalDensity.current
+
+    LaunchedEffect(Unit) {
+        with(density) {
+            listState.scrollToItem(
+                index = 1,
+                scrollOffset = (-70).dp.roundToPx()
+            )
+        }
+    }
+
+    LazyRow(
+        modifier = modifier,
+        state = listState,
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        MembershipScreenCard(
-            modifier = Modifier,
-            isRecommended = isRecommended,
-            membership = Membership.FREE
-        )
-
-        MembershipScreenCard(
-            modifier = Modifier,
-            membership = Membership.PREMIUM,
-            isRecommended = true,
-            isDiscounted = true,
-            discount = 25.0
-        )
-
-        MembershipScreenCard(
-            modifier = Modifier,
-            membership = Membership.ULTRA,
-        )
+        item {
+            MembershipScreenCard(
+                modifier = Modifier,
+                membership = Membership.FREE
+            )
+        }
+        item {
+            MembershipScreenCard(
+                modifier = Modifier,
+                membership = Membership.PREMIUM,
+                isRecommended = true,
+                isDiscounted = true,
+                discount = 25.0
+            )
+        }
+        item {
+            MembershipScreenCard(
+                modifier = Modifier,
+                membership = Membership.ULTRA,
+            )
+        }
     }
 }
 
@@ -52,7 +67,6 @@ fun MembershipScreenBody(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun MembershipScreenBodyPreview() {
-    val benefitItems = MembershipBenefitsDataSource.getBenefits(Membership.PREMIUM)
     ChatEaseTheme {
         Scaffold { paddingValues ->
             Column(
@@ -62,9 +76,7 @@ private fun MembershipScreenBodyPreview() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                MembershipScreenBody(
-                    isRecommended = false,
-                )
+                MembershipScreenBody()
             }
         }
     }
