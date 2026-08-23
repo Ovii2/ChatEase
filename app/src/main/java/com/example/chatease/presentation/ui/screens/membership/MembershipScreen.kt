4bc18143165473717629c24341dcced0ae390377
Ovidijus <ovidijus.e@gmail.com>
2026-08-23 09:51:44 +0300

    update alignment of screen content

--- app/src/main/java/com/example/chatease/presentation/ui/screens/membership/MembershipScreen.kt
+++ app/src/main/java/com/example/chatease/presentation/ui/screens/membership/MembershipScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,6 +40,7 @@ fun MembershipScreen(
         ) {
             MembershipScreenContent(
                 modifier = Modifier
+                    .widthIn(max = 1000.dp)
                     .fillMaxWidth()
             )
         }
